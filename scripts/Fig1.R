c75ce8dd633db85e85b556f87e77535ff16dec97
ggloor <ggloor@uwo.ca>
2016-07-06 16:20:36 -0400

    Fig 1 is base graphics and AB

--- scripts/Fig1.R
+++ scripts/Fig1.R
@@ -1,121 +1,101 @@
-################################################################################ 
+################################################################################
 ##### Fig1.R
 ##### Author: Jia Rong Wu
-##### jwu424 (at) gmail.com 
+##### jwu424 (at) gmail.com
 #####
-##### DESCRIPTION: Generalized R script in order to generate supporting figures 
+##### DESCRIPTION: Generalized R script in order to generate supporting figures
 ##### for the paper IQLR. Code for Figure 1 a/b.
-##### 
+#####
 ##### USAGE: Rscript --vanilla Fig1.R
-##### 
-##### LICENSE 
+#####
+##### LICENSE
 ##### Copyright (c) 2016 Jia Rong Wu
-##### 
-##### Permission is hereby granted, free of charge, to any person obtaining a 
-##### copy of this software and associated documentation files (the "Software"), 
-##### to deal in the Software without restriction, including without limitation 
-##### the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-##### and/or sell copies of the Software, and to permit persons to whom the 
+#####
+##### Permission is hereby granted, free of charge, to any person obtaining a
+##### copy of this software and associated documentation files (the "Software"),
+##### to deal in the Software without restriction, including without limitation
+##### the rights to use, copy, modify, merge, publish, distribute, sublicense,
+##### and/or sell copies of the Software, and to permit persons to whom the
 ##### Software is furnished to do so, subject to the following conditions:
-##### 
-##### The above copyright notice and this permission notice shall be included in 
+#####
+##### The above copyright notice and this permission notice shall be included in
 ##### all copies or substantial portions of the Software.
-##### 
-##### THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-##### IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-##### FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-##### THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-##### LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-##### FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+#####
+##### THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+##### IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+##### FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+##### THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+##### LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+##### FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 ##### DEALINGS IN THE SOFTWARE.
-################################################################################ 
+################################################################################
 
 ##### Load Required Libraries
-source("Variables.R")
+source("scripts/Variables.R")
 
 ################################### FIGURE 1a ##################################
 ##### This figure is generated off dataset reads
 ##### It is the original unmodified base dataset with 40 positive controls
 ##### This is original ALDEx2 CLR
 ################################### FIGURE 1a ##################################
-##### Read file and ensure it exists
-file.name <- "reads"
-read.file <- paste(data.dir, file.name, sep="")
-if (!file.exists(read.file)){
-	stop(paste("File: '",file.name,"' does not exist.",sep=""), call.=FALSE)}
 
 ##### Read table and generate conditions
-reads <- read.table(read.file, header=T, row.names=1, sep="\t", check.names=F)
-x.clr.o <- aldex.clr(reads, conds, mc.samples, zero=FALSE, verbose=FALSE, useMC=FALSE)
+x.clr <- aldex.clr(reads, mc.samples, verbose=FALSE, useMC=FALSE)
 conds <- c(rep("A", 10), rep("B", 10))
-x.e.o <- aldex.effect(x.clr.o, conds, useMC=TRUE) 
-x.t.o  <- aldex.ttest(x.clr.o, conds)
-x.all.o <- data.frame(x.e.o, x.t.o)
+x.e <- aldex.effect(x.clr, conds, useMC=TRUE)
+x.t  <- aldex.ttest(x.clr, conds)
+x.all <- data.frame(x.e, x.t)
+
+##### make assymmetric dataset
+reads.0 <- reads
+reads.0[5800:5859,1:10] <- 0
+
+x.clr.0 <- aldex.clr(reads.0, mc.samples, verbose=FALSE, useMC=FALSE)
+x.e.0 <- aldex.effect(x.clr.0, conds, useMC=TRUE)
+x.t.0  <- aldex.ttest(x.clr.0, conds)
+x.all.0 <- data.frame(x.e.0, x.t.0)
 
 
-f1a <- paste(figs.dir, "Fig_1a.png",sep="")
-png(f1a)
+f1 <- paste(figs.dir, "Fig_1.pdf",sep="")
 
-plot.new()
-	pushViewport(viewport())
-	called <- x.all.o$we.eBH <= cutoff
-	plot(x.all.o$diff.win, x.all.o$diff.btw, xlab=xlab, ylab=ylab, col=all.col, pch=all.pch, cex=all.cex, main="IQR-Unadjusted Effect Plot", ylim=c(ymin,ymax))
-	points(x.all.o$diff.win[x.all.o$rab.all < rare], x.all.o$diff.btw[x.all.o$rab.all < rare], col=rare.col, pch=rare.pch, cex=rare.cex)
-	points(x.all.o$diff.win[called], x.all.o$diff.btw[called], col=called.col, pch=called.pch, cex=called.cex)
+pdf(f1, height=6, width=11)
+par(fig=c(0,1,0,1), new=TRUE)
+par(fig=c(0,0.5,0,1), new=TRUE)
+	called <- x.all$we.eBH <= cutoff
+	plot(x.all$diff.win, x.all$diff.btw, xlab=xlab, ylab=ylab, col=all.col, pch=all.pch, cex=all.cex, main="IQR-Unadjusted Effect Plot", ylim=c(ymin,ymax))
+	points(x.all$diff.win[x.all$rab.all < rare], x.all$diff.btw[x.all$rab.all < rare], col=rare.col, pch=rare.pch, cex=rare.cex)
+	points(x.all$diff.win[called], x.all$diff.btw[called], col=called.col, pch=called.pch, cex=called.cex)
+	points(x.all$diff.win[called], x.all$diff.btw[called], col=called.col, pch=called.pch, cex=called.cex)
 	abline(0,1, col=thres.line.col, lty=2, lwd=thres.lwd)
 	abline(0,-1, col=thres.line.col, lty=2, lwd=thres.lwd)
 	abline(0,0, col="black")
 	abline(0,0, col="white", lwd=thres.lwd, lty=2)
-	pushViewport(viewport(x=.2,y=.8,width=.25,height=.25,just=c("left","top")))
-	grid.rect()
-	par(plt = gridPLT(), new=TRUE)
-	hist(x.all.o$diff.btw, breaks=500, xlim=c(-1,1), main=expression( "Median" ~~ Log[2] ~~ "btw-Condition diff" ), xlab="", ylab="", cex.main=0.8)
+par(fig=c(0.05,0.25, 0.4,0.9), new=TRUE)
+	hist(x.all$diff.btw, breaks=500, xlim=c(-1,1), main=expression( "Median" ~~ Log[2] ~~ "btw-Condition diff" ), xlab="", ylab="", cex.main=0.6)
 	abline(v=0, col="black", lwd=thres.lwd)
 	abline(v=0, col="white", lwd=thres.lwd, lty=2)
-popViewport(2)
-
-dev.off()
 
 ################################### FIGURE 1b ##################################
 ##### This figure is generated off dataset reads_2
 ##### It is a dataset with 120 features in condition A that have 0's introduced
-##### This is the original ALDEx2 CLR 
+##### This is the original ALDEx2 CLR
 ################################### FIGURE 1b ##################################
-##### Read file and ensure it exists
-file.name <- "reads_2"
-read.file <- paste(data.dir, file.name, sep="")
-if (!file.exists(read.file)){
-	stop(paste("File: '",file.name,"' does not exist.",sep=""), call.=FALSE)}
 
-##### Read table and generate conditions
-reads <- read.table(read.file, header=T, row.names=1, sep="\t", check.names=F)
-conds <- c(rep("A", 10), rep("B", 10))
-x.clr.o <- aldex.clr(reads, conds, mc.samples, zero=FALSE, verbose=FALSE, useMC=FALSE)
-x.e.o <- aldex.effect(x.clr.o, conds, useMC=TRUE) 
-x.t.o  <- aldex.ttest(x.clr.o, conds)
-x.all.o <- data.frame(x.e.o, x.t.o)
-
-
-f1b <- paste(figs.dir, "Fig_1b.png",sep="")
-png(f1b)
-
-plot.new()
-	pushViewport(viewport())
-	called <- x.all.o$we.eBH <= cutoff
-	plot(x.all.o$diff.win, x.all.o$diff.btw, xlab=xlab, ylab=ylab, col=all.col, pch=all.pch, cex=all.cex, main="IQR-Unadjusted Effect Plot", ylim=c(ymin,ymax))
-	points(x.all.o$diff.win[x.all.o$rab.all < rare], x.all.o$diff.btw[x.all.o$rab.all < rare], col=rare.col, pch=rare.pch, cex=rare.cex)
-	points(x.all.o$diff.win[called], x.all.o$diff.btw[called], col=called.col, pch=called.pch, cex=called.cex)
+par(fig=c(0.5,1,0,1), new=TRUE)
+	called <- x.all.0$we.eBH <= cutoff
+	plot(x.all.0$diff.win, x.all.0$diff.btw, xlab=xlab, ylab=ylab, col=all.col, pch=all.pch, cex=all.cex, main="IQR-Unadjusted Effect Plot", ylim=c(ymin,ymax))
+	points(x.all.0$diff.win[x.all.0$rab.all < rare], x.all.0$diff.btw[x.all.0$rab.all < rare], col=rare.col, pch=rare.pch, cex=rare.cex)
+	points(x.all.0$diff.win[called], x.all.0$diff.btw[called], col=called.col, pch=called.pch, cex=called.cex)
 	abline(0,1, col=thres.line.col, lty=2, lwd=thres.lwd)
 	abline(0,-1, col=thres.line.col, lty=2, lwd=thres.lwd)
 	abline(0,0, col="black")
 	abline(0,0, col="white", lwd=thres.lwd, lty=2)
-	pushViewport(viewport(x=.2,y=.8,width=.25,height=.25,just=c("left","top")))
-	grid.rect()
-	par(plt = gridPLT(), new=TRUE)
-	hist(x.all.o$diff.btw, breaks=500, xlim=c(-1,1), main=expression( "Median" ~~ Log[2] ~~ "btw-Condition diff" ), xlab="", ylab="", cex.main=0.8)
+
+par(fig=c(0.55,0.75, 0.4,0.9), new=TRUE)
+	hist(x.all.0$diff.btw, breaks=500, xlim=c(-1,1), main=expression( "Median" ~~ Log[2] ~~ "btw-Condition diff" ), xlab="", ylab="", cex.main=0.6)
 	abline(v=0, col="black", lwd=thres.lwd)
 	abline(v=0, col="white", lwd=thres.lwd, lty=2)
-popViewport(2)
+#popViewport(2)
 
 dev.off()
 
--- scripts/Variables.R
+++ scripts/Variables.R
@@ -1,39 +1,39 @@
-################################################################################ 
+################################################################################
 ##### Variables.R
 ##### Author: Jia Rong Wu
-##### jwu424 (at) gmail.com 
+##### jwu424 (at) gmail.com
 #####
-##### DESCRIPTION: R script that loads variables and plotting variables common 
+##### DESCRIPTION: R script that loads variables and plotting variables common
 ##### to each figures script for breveity.
-##### 
+#####
 ##### USAGE: At the beginning of each script that requires these variables,
 ##### append the following line: source("Variables.R")
-##### 
-##### LICENSE 
+#####
+##### LICENSE
 ##### Copyright (c) 2016 Jia Rong Wu
-##### 
-##### Permission is hereby granted, free of charge, to any person obtaining a 
-##### copy of this software and associated documentation files (the "Software"), 
-##### to deal in the Software without restriction, including without limitation 
-##### the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-##### and/or sell copies of the Software, and to permit persons to whom the 
+#####
+##### Permission is hereby granted, free of charge, to any person obtaining a
+##### copy of this software and associated documentation files (the "Software"),
+##### to deal in the Software without restriction, including without limitation
+##### the rights to use, copy, modify, merge, publish, distribute, sublicense,
+##### and/or sell copies of the Software, and to permit persons to whom the
 ##### Software is furnished to do so, subject to the following conditions:
-##### 
-##### The above copyright notice and this permission notice shall be included in 
+#####
+##### The above copyright notice and this permission notice shall be included in
 ##### all copies or substantial portions of the Software.
-##### 
-##### THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-##### IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-##### FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-##### THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-##### LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-##### FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+#####
+##### THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+##### IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+##### FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+##### THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+##### LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+##### FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 ##### DEALINGS IN THE SOFTWARE.
-################################################################################ 
+################################################################################
 
 ##### Setup correct directories
-data.dir <- "../data/"
-figs.dir <- "../figures/"
+data.dir <- "data/"
+figs.dir <- "figures/"
 
 ##### Load Required Libraries
 library(ALDEx2)
@@ -55,14 +55,32 @@ rdirichlet <- function (n, alpha)
   return(x / rowSums(x))
 }
 
+# this is the dataset
+# can easily remake the raw data by introducing 0 or 1 count variables in
+# one or other subset at runtime
+##### variables 47-56 5X
+##### variables 57-66 4X
+##### variables 67-76 3X
+##### variables 77-86 2X
+##### variable 108 is FP effect >1, variables 1679, 3671 fp effect < -1:  just over threshold by chance
+file.name <- "d.n0.txt" # this is the original reads file with 0 count variables removed
+read.file <- paste(data.dir, file.name, sep="")
+
+if (!file.exists(read.file)){
+	stop(paste("File: '",file.name,"' does not exist.",sep=""), call.=FALSE)}
+
+reads <- read.table(read.file, header=T, row.names=1, sep="\t", check.names=F)
+true.set <- rownames(reads[47:86,])
+fp.set <- rownames(reads[c(108,1679,3671),])
+
 upper.bound <- 4
 lower.bound <- 2
 verbose <- FALSE
 useMC <- FALSE
 include.sample.summary <- FALSE
-mc.samples <- 8
+mc.samples <- 16
 
-##### Plotting Variable Declarations 
+##### Plotting Variable Declarations
 xlab=NULL
 ylab=NULL
 xlim=NULL
@@ -75,7 +93,7 @@ called.pch=20
 called.cex=0.6
 thres.line.col="darkgrey"
 thres.lwd=1.5
-test="welch"
+test="wilcox"
 cutoff=0.1
 rare.col="black"
 rare=0
